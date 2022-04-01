98c99273837384a1d80ba642de4d64f792f7f7ba
Manuel Duarte <carlosd99m@gmail.com>
2022-04-01 16:04:54 -0600

    :sparkles: feat(android): geolocator

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -14,5 +15,30 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         isGpsPermissionGranted: event.isGpsPermissionGranted,
       ));
     });
+
+    _init();
+  }
+
+// El va escuchar el evento de los permisos
+  Future<void> _init() async {
+    _checkGpsStatus();
+  }
+
+  // para saber si esta activado el gps
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
+      print('Service status: $isEnabled');
+    });
+
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: Limpiar ServiceStatus stream
+    return super.close();
   }
 }
