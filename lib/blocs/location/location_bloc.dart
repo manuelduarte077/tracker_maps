4da1d7823be9077ec457e5ff9cf009a606a4d2a3
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-04 11:22:27 -0600

    fix: changing the state of the block

--- lib/blocs/location/location_bloc.dart
+++ lib/blocs/location/location_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
 
 part 'location_event.dart';
 part 'location_state.dart';
@@ -11,20 +12,28 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
   StreamSubscription<Position>? positionStream;
 
   LocationBloc() : super(const LocationState()) {
-    on<LocationEvent>((event, emit) {});
+    on<OnNewUserLocationEvent>((event, emit) {
+      emit(state.copyWith(
+        lastKnownLocation: event.newLocation,
+        myLocationHistory: [...state.myLocationHistory, event.newLocation],
+      ));
+    });
   }
 
   Future getCurrentLocation() async {
     final position = await Geolocator.getCurrentPosition();
-    print('Position: $position');
-    // TODDO: retornar unj objeto de tipo LatLng
+    add(
+      OnNewUserLocationEvent(LatLng(position.latitude, position.longitude)),
+    );
   }
 
   void startFollowingUser() {
     print('startFollowingUser');
     positionStream = Geolocator.getPositionStream().listen((event) {
       final position = event;
-      print('Position: $position');
+      add(
+        OnNewUserLocationEvent(LatLng(position.latitude, position.longitude)),
+      );
     });
   }
 
--- lib/blocs/location/location_event.dart
+++ lib/blocs/location/location_event.dart
@@ -6,3 +6,10 @@ abstract class LocationEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+// Evento para recibir la nueva ubicación del usuario
+class OnNewUserLocationEvent extends LocationEvent {
+  final LatLng newLocation;
+
+  const OnNewUserLocationEvent(this.newLocation);
+}
