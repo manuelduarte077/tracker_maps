e6f90c2074f5a5e130c993b9590106e3807a92c1
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-10 11:37:28 -0600

    feat: adding the splashscreen and icon launcher

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maps/screens/screens.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,6 +11,32 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       home: const LoadingScreen(),
+    );
+  }
+}
+
+class SplasScreenApp extends StatelessWidget {
+  const SplasScreenApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreenView(
+      navigateRoute: const LoadingScreen(),
+      duration: 5000,
+      imageSize: 130,
+      imageSrc: 'assets/icon/logo_app.png',
+      text: 'Splash Screen',
+      textType: TextType.ColorizeAnimationText,
+      textStyle: const TextStyle(
+        fontSize: 40.0,
+      ),
+      colors: const [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.white,
     );
   }
 }
