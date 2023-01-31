b2ef3fa8d8bed0da26e7c9fbacabdeea3cea0895
Manuel Duarte  <carlosd99m@gmail.com>
2023-01-31 10:08:37 -0600

    chore(flutter): Update to Dart 2.19 and latest packages

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:maps/screens/screens.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -13,32 +12,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const LoadingScreen(),
-    );
-  }
-}
-
-class SplasScreenApp extends StatelessWidget {
-  const SplasScreenApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreenView(
-      navigateRoute: const LoadingScreen(),
-      duration: 5000,
-      imageSize: 130,
-      imageSrc: 'assets/icon/logo_app.png',
-      text: 'Splash Screen',
-      textType: TextType.ColorizeAnimationText,
-      textStyle: const TextStyle(
-        fontSize: 40.0,
-      ),
-      colors: const [
-        Colors.purple,
-        Colors.blue,
-        Colors.yellow,
-        Colors.red,
-      ],
-      backgroundColor: Colors.white,
     );
   }
 }
